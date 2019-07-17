d771057d388fbcd23d4d7149a7e874a528c95281
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-07-17 10:47:26 +0200

    fix: typos in sanitize unit test

--- test/unit/helpers/sanitization/sanitizer.ts
+++ test/unit/helpers/sanitization/sanitizer.ts
@@ -2,7 +2,7 @@ import { presets, sanitize } from '../../../../src/helpers/sanitization';
 
 describe('Sanitizer helper', () => {
 	it('Should remove all unallowed tags when using the `basic` preset', () => {
-		const input = '<hmtl><p>Hello! <a href="https://www.test.com">www.test.com</a></p>  <span>testing</span> <ul><li>item</li></ul></html>';
+		const input = '<html><p>Hello! <a href="https://www.test.com">www.test.com</a></p>  <span>testing</span> <ul><li>item</li></ul></html>';
 		const expectedOutput = '<p>Hello! www.test.com</p>  testing <ul><li>item</li></ul>';
 
 		const output = sanitize(input, presets.basic);
@@ -10,8 +10,8 @@ describe('Sanitizer helper', () => {
 		expect(output).toEqual(expectedOutput);
 	});
 
-	it('Should  remove all unallowed tags and attributes when using the `link` preset', () => {
-		const input = '<hmtl><p>Hello! <a href="https://www.test.com" rel="noreferrer">www.test.com</a></p>  <span>testing</span> <ul><li>item</li></ul></html>';
+	it('Should remove all unallowed tags and attributes when using the `link` preset', () => {
+		const input = '<html><p>Hello! <a href="https://www.test.com" rel="noreferrer">www.test.com</a></p>  <span>testing</span> <ul><li>item</li></ul></html>';
 		const expectedOutput = '<p>Hello! <a href="https://www.test.com">www.test.com</a></p>  testing <ul><li>item</li></ul>';
 
 		const output = sanitize(input, presets.links);
