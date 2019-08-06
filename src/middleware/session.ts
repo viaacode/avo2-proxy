ed0bf29043791eaa2c54f397b6ef1c26c14b189c
Bert Verhelst <verhelstbert@gmail.com>
2019-08-06 10:55:25 +0200

    chore: remove commented session postgres code + add todo

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -9,21 +9,8 @@ const pgSession = connectPgSimple(session);
 if (!process.env.COOKIES_SECRET) {
 	console.error(new RecursiveError('COOKIES_SECRET env variable is required'));
 }
-// export default session({
-// 	store: new pgSession({
-// 		pool: pgPool,
-// 		schemaName: 'app',
-// 		tableName : 'session',
-// 	}),
-// 	resave: false, // Postgres session provider doesn't need to resave the session every time
-// 	saveUninitialized: true, // Do not create a session for users that are not logged in, neither for health checks
-// 	cookie: {
-// 		secure: false, // TODO make secure once app runs on https: https://www.npmjs.com/package/express-session#cookiesecure
-// 		httpOnly: false,
-// 		maxAge: parseInt(process.env.COOKIES_MAXAGE as string, 10),
-// 	},
-// 	secret: process.env.COOKIES_SECRET || '',
-// });
+
+// TODO store sessions in redis instance provided by VIAA
 
 export default session({
 	resave: false, // Postgres session provider doesn't need to resave the session every time
