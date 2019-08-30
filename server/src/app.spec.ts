f469c20ba620c1ec07c0334956f7436725ec6024
Bert Verhelst <verhelstbert@gmail.com>
2019-08-30 14:16:38 +0200

    fix: enable app.spec tests

--- server/src/app.spec.ts
+++ server/src/app.spec.ts
@@ -51,111 +51,111 @@ describe('[UNIT - APP] Application', () => {
 		app.server.close();
 		done();
 	});
-	//
-	// it('Should start an Express server and error', async (done: jest.DoneCallback) => {
-	// 	const error: Error = new Error();
-	// 	app.app.listen = ((port: number, callback: (err?: Error) => void) => {
-	// 		callback(error);
-	// 	}) as any; // tslint:disable-line no-any
-	// 	app.start();
-	//
-	// 	await wait(500);
-	//
-	// 	expect(loggerErrorSpy).toHaveBeenCalledWith(error);
-	// 	expect(exitProcessSpy).toHaveBeenCalledWith(1);
-	//
-	// 	done();
-	// });
-	//
-	// it('Should stop the Express server on SIGTERM', async (done: jest.DoneCallback) => {
-	// 	app.start();
-	//
-	// 	await wait(500);
-	//
-	// 	closeServerSpy = jest.spyOn(app.server, 'close');
-	//
-	// 	app.stop('SIGTERM');
-	//
-	// 	await wait(500);
-	//
-	// 	expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGTERM signal, graceful shutdown');
-	// 	expect(closeServerSpy).toHaveBeenCalled();
-	// 	expect(exitProcessSpy).toHaveBeenCalled();
-	//
-	// 	closeServerSpy.mockRestore();
-	// 	done();
-	// });
-	//
-	// it('Should stop the Express server on SIGTERM and error', async (done: jest.DoneCallback) => {
-	// 	app.start();
-	// 	const error: Error = new Error();
-	// 	const close: Function = app.server.close;
-	// 	app.server.close = ((callback: (err?: Error) => void) => {
-	// 		callback(error);
-	// 	}) as any; // tslint:disable-line no-any
-	//
-	// 	await wait(500);
-	//
-	// 	closeServerSpy = jest.spyOn(app.server, 'close');
-	//
-	// 	app.stop('SIGTERM');
-	//
-	// 	await wait(500);
-	//
-	// 	expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGTERM signal, graceful shutdown');
-	// 	expect(closeServerSpy).toHaveBeenCalled();
-	// 	expect(loggerErrorSpy).toHaveBeenCalledWith(error);
-	// 	expect(exitProcessSpy).toHaveBeenCalledWith(1);
-	//
-	// 	closeServerSpy.mockRestore();
-	// 	app.server.close = close as any; // tslint:disable-line no-any
-	// 	app.server.close();
-	// 	done();
-	// });
-	//
-	// it('Should stop the Express server on SIGINT', async (done: jest.DoneCallback) => {
-	// 	app.start();
-	//
-	// 	await wait(500);
-	//
-	// 	closeServerSpy = jest.spyOn(app.server, 'close');
-	//
-	// 	app.stop('SIGINT');
-	//
-	// 	await wait(500);
-	//
-	// 	expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGINT signal, graceful shutdown');
-	// 	expect(closeServerSpy).toHaveBeenCalled();
-	// 	expect(exitProcessSpy).toHaveBeenCalled();
-	//
-	// 	closeServerSpy.mockRestore();
-	// 	done();
-	// });
-	//
-	// it('Should stop the Express server on SIGINT and error', async (done: jest.DoneCallback) => {
-	// 	app.start();
-	// 	const error: Error = new Error();
-	// 	const close: Function = app.server.close;
-	// 	app.server.close = ((callback: (err?: Error) => void) => {
-	// 		callback(error);
-	// 	}) as any; // tslint:disable-line no-any
-	//
-	// 	await wait(500);
-	//
-	// 	closeServerSpy = jest.spyOn(app.server, 'close');
-	//
-	// 	app.stop('SIGINT');
-	//
-	// 	await wait(500);
-	//
-	// 	expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGINT signal, graceful shutdown');
-	// 	expect(closeServerSpy).toHaveBeenCalled();
-	// 	expect(loggerErrorSpy).toHaveBeenCalledWith(error);
-	// 	expect(exitProcessSpy).toHaveBeenCalledWith(1);
-	//
-	// 	closeServerSpy.mockRestore();
-	// 	app.server.close = close as any; // tslint:disable-line no-any
-	// 	app.server.close();
-	// 	done();
-	// });
+
+	it('Should start an Express server and error', async (done: jest.DoneCallback) => {
+		const error: Error = new Error();
+		app.app.listen = ((port: number, callback: (err?: Error) => void) => {
+			callback(error);
+		}) as any; // tslint:disable-line no-any
+		app.start();
+
+		await wait(500);
+
+		expect(loggerErrorSpy).toHaveBeenCalledWith(error);
+		expect(exitProcessSpy).toHaveBeenCalledWith(1);
+
+		done();
+	});
+
+	it('Should stop the Express server on SIGTERM', async (done: jest.DoneCallback) => {
+		app.start();
+
+		await wait(500);
+
+		closeServerSpy = jest.spyOn(app.server, 'close');
+
+		app.stop('SIGTERM');
+
+		await wait(500);
+
+		expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGTERM signal, graceful shutdown');
+		expect(closeServerSpy).toHaveBeenCalled();
+		expect(exitProcessSpy).toHaveBeenCalled();
+
+		closeServerSpy.mockRestore();
+		done();
+	});
+
+	it('Should stop the Express server on SIGTERM and error', async (done: jest.DoneCallback) => {
+		app.start();
+		const error: Error = new Error();
+		const close: Function = app.server.close;
+		app.server.close = ((callback: (err?: Error) => void) => {
+			callback(error);
+		}) as any; // tslint:disable-line no-any
+
+		await wait(500);
+
+		closeServerSpy = jest.spyOn(app.server, 'close');
+
+		app.stop('SIGTERM');
+
+		await wait(500);
+
+		expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGTERM signal, graceful shutdown');
+		expect(closeServerSpy).toHaveBeenCalled();
+		expect(loggerErrorSpy).toHaveBeenCalledWith(error);
+		expect(exitProcessSpy).toHaveBeenCalledWith(1);
+
+		closeServerSpy.mockRestore();
+		app.server.close = close as any; // tslint:disable-line no-any
+		app.server.close();
+		done();
+	});
+
+	it('Should stop the Express server on SIGINT', async (done: jest.DoneCallback) => {
+		app.start();
+
+		await wait(500);
+
+		closeServerSpy = jest.spyOn(app.server, 'close');
+
+		app.stop('SIGINT');
+
+		await wait(500);
+
+		expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGINT signal, graceful shutdown');
+		expect(closeServerSpy).toHaveBeenCalled();
+		expect(exitProcessSpy).toHaveBeenCalled();
+
+		closeServerSpy.mockRestore();
+		done();
+	});
+
+	it('Should stop the Express server on SIGINT and error', async (done: jest.DoneCallback) => {
+		app.start();
+		const error: Error = new Error();
+		const close: Function = app.server.close;
+		app.server.close = ((callback: (err?: Error) => void) => {
+			callback(error);
+		}) as any; // tslint:disable-line no-any
+
+		await wait(500);
+
+		closeServerSpy = jest.spyOn(app.server, 'close');
+
+		app.stop('SIGINT');
+
+		await wait(500);
+
+		expect(loggerInfoSpy).toHaveBeenCalledWith('Server stopped due to SIGINT signal, graceful shutdown');
+		expect(closeServerSpy).toHaveBeenCalled();
+		expect(loggerErrorSpy).toHaveBeenCalledWith(error);
+		expect(exitProcessSpy).toHaveBeenCalledWith(1);
+
+		closeServerSpy.mockRestore();
+		app.server.close = close as any; // tslint:disable-line no-any
+		app.server.close();
+		done();
+	});
 });
