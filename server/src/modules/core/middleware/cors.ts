a7b4802781c9ecf5123521d9502ca03d14cda7e5
Bert Verhelst <verhelstbert@gmail.com>
2019-10-14 15:20:35 +0200

    chore(cors): cleanup code

--- server/src/modules/core/middleware/cors.ts
+++ server/src/modules/core/middleware/cors.ts
@@ -14,8 +14,8 @@ const WHITE_LIST_DOMAINS = [
 	'https://avo2-client-qas.hetarchief.be',
 	'https://avo2-client-prd.hetarchief.be',
 	'https://avo2-client.hetarchief.be',
-	'https://hetarchief.be',
 	'https://onderwijs.hetarchief.be',
+	'https://hetarchief.be',
 ];
 
 export default function (req: Request, res: Response, next: NextFunction) {
@@ -24,30 +24,10 @@ export default function (req: Request, res: Response, next: NextFunction) {
 			if (WHITE_LIST_DOMAINS.indexOf(origin) !== -1 || (!origin && (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'development'))) {
 				callback(null, true);
 			} else {
-				callback(new Error('Not allowed by CORS'));
+				callback(new Error(`Domain: "${origin}" has not been whitelisted`));
 			}
 		},
 		allowedHeaders: 'X-Requested-With, Content-Type, authorization, Origin, Accept, cache-control',
 		methods: 'GET, POST, OPTIONS, PUT, DELETE',
 	});
-	// res.header('Access-Control-Allow-Origin', '*');
-	// res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, authorization, Origin, Accept, cache-control');
-	// res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	// // Intercepts OPTIONS method
-	// if (req.method === 'OPTIONS') {
-	// 	// Respond with 200
-	// 	res.sendStatus(200);
-	// } else {
-	// 	// Move on
-	// 	next();
-	// }
 }
-
-// TODO limit domains to specific list:
-// [
-// 		'http://localhost:8080',
-// 		'http://avo2-client-dev-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
-// 		'http://avo2-client-tst-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
-// 		'http://avo2-client-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
-// 		'http://avo2-client-prd-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
-// 	]
