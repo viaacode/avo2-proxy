8f3b0b523cde8a98b9a42bb02dbe8d7d070bceed
Bert Verhelst <verhelstbert@gmail.com>
2019-10-14 13:58:42 +0200

    fix(cors): add whitelist for cors domains (wip)

--- server/src/modules/core/middleware/cors.ts
+++ server/src/modules/core/middleware/cors.ts
@@ -1,17 +1,46 @@
 import { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+const WHITE_LIST_DOMAINS = [
+	'http://localhost:8080',
+	'http://avo2-client-dev-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
+	'http://avo2-client-int-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
+	'http://avo2-client-tst-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
+	'http://avo2-client-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
+	'http://avo2-client-prd-sc-avo2.apps.do-prd-okp-m0.do.viaa.be',
+	'https://avo2-client-dev.hetarchief.be',
+	'https://avo2-client-int.hetarchief.be',
+	'https://avo2-client-tst.hetarchief.be',
+	'https://avo2-client-qas.hetarchief.be',
+	'https://avo2-client-prd.hetarchief.be',
+	'https://avo2-client.hetarchief.be',
+	'https://hetarchief.be',
+	'https://onderwijs.hetarchief.be',
+];
 
 export default function (req: Request, res: Response, next: NextFunction) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, authorization, Origin, Accept, cache-control');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	// Intercepts OPTIONS method
-	if (req.method === 'OPTIONS') {
-		// Respond with 200
-		res.sendStatus(200);
-	} else {
-		// Move on
-		next();
-	}
+	cors({
+		origin: (origin, callback) => {
+			if (WHITE_LIST_DOMAINS.indexOf(origin) !== -1 || (!origin && (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'development'))) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		},
+		allowedHeaders: 'X-Requested-With, Content-Type, authorization, Origin, Accept, cache-control',
+		methods: 'GET, POST, OPTIONS, PUT, DELETE',
+	});
+	// res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, authorization, Origin, Accept, cache-control');
+	// res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	// // Intercepts OPTIONS method
+	// if (req.method === 'OPTIONS') {
+	// 	// Respond with 200
+	// 	res.sendStatus(200);
+	// } else {
+	// 	// Move on
+	// 	next();
+	// }
 }
 
 // TODO limit domains to specific list:
