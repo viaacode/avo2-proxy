9d2a12a317f2a5632507f7ce45dbdeff189a43b6
Bert Verhelst <verhelstbert@gmail.com>
2019-10-29 19:00:39 +0100

    feat(related-items): implement related items route for items and coll.

--- server/src/modules/search/controller.ts
+++ server/src/modules/search/controller.ts
@@ -36,4 +36,42 @@ export default class SearchController {
 			}
 		}
 	}
+
+	public static async getRelatedItems(itemId: string, index: EsIndex, limit: number = 5): Promise<Avo.Search.Search> {
+		try {
+			// Convert filters to ElasticSearch query object using queryBuilder
+			const esQueryObject = {
+				size: limit,
+				from: 0,
+				query: {
+					more_like_this : {
+						fields : ['dc_title', 'dcterms_abstract'],
+						like : [
+							{
+								_index : ES_INDEX_MAP[index],
+								_id : itemId,
+							},
+						],
+						min_term_freq : 1,
+						max_query_terms : 12,
+					},
+				},
+			};
+
+			// Perform search
+			return await SearchService.search(esQueryObject, ES_INDEX_MAP[index || 'items']);
+		} catch (err) {
+			if (err.statusText === 'Bad Request') {
+				throw new CustomError(
+					'Failed to do search, are you connected to the elasticsearch VPN?',
+					err,
+					{ itemId, index, limit });
+			} else {
+				throw new CustomError(
+					'Failed to do search',
+					err,
+					{ itemId, index, limit });
+			}
+		}
+	}
 }
