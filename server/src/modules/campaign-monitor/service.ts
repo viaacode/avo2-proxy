3297cbaf95376bb535141c4ec0a65ec89aadde88
Bert Verhelst <verhelstbert@gmail.com>
2020-02-06 11:04:09 +0100

    fix(campaign-monitor): remove curl comment containing viaa user name

--- server/src/modules/campaign-monitor/service.ts
+++ server/src/modules/campaign-monitor/service.ts
@@ -10,25 +10,6 @@ checkRequiredEnvs([
 	'CAMPAIGN_MONITOR_API_KEY',
 ]);
 
-/*
-
-curl -u YOUR_API_KEY:. -H "Content-Type: application/json" \
--X POST -d '{
-    "To": ["Claire Denoyette <claire.denoyette@viaa.be>"],
-    "Data": {
-        "gebruikersnaam": "gebruikersnaamTestValue",
-        "titel": "titelTestValue",
-        "x-apple-data-detectors": "x-apple-data-detectorsTestValue",
-        "href": "hrefTestValue",
-        "style*=\"font-size:1px\"": "style*=\"font-size:1px\"TestValue",
-        "link-asset": "link-assetTestValue"
-    },
-    "ConsentToTrack": "no|yes|unchanged"
-}' \
-/4293ab4f-40a9-47ae-bb17-32edb593c3ba/send
-
- */
-
 export default class CampaignMonitorService {
 	/**
 	 * Send an email using the campaign monitor api
