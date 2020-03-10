a9e043af278319a73ea9a62f1c4b944fb2e6caad
Benjamin Naesen <benjamin@naesen.be>
2020-03-10 14:11:44 +0100

    refactor(site-variables): import order

--- server/src/modules/site-variables/controllers/translations.controller.ts
+++ server/src/modules/site-variables/controllers/translations.controller.ts
@@ -1,11 +1,12 @@
 import { get } from 'lodash';
 
-import DataService from '../../data/service';
-import { GET_SITE_VARIABLES_BY_NAME } from '../queries.gql';
 import {
 	ExternalServerError,
 	NotFoundError,
 } from '../../../shared/helpers/error';
+import DataService from '../../data/service';
+
+import { GET_SITE_VARIABLES_BY_NAME } from '../queries.gql';
 import { SiteVariable } from '../types';
 
 export default class TranslationsController {
