b0683078e437ad579cd181dd6a7ee27cf524db6e
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-10 11:53:55 +0100

    fix: add missing content_width property for content pages

--- server/src/modules/navigation-items/queries.gql.ts
+++ server/src/modules/navigation-items/queries.gql.ts
@@ -23,6 +23,7 @@ export const GET_CONTENT_PAGE_BY_PATH = `
 		app_content(where: { path: { _eq: $path } }) {
 			title
 			content_type
+			content_width
 			created_at
 			depublish_at
 			description
