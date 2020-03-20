00099961ea8d8826c6e4ffb452e3295238c4f70f
Bert Verhelst <verhelstbert@gmail.com>
2020-03-20 15:38:50 +0100

    feat(sessions): store sessions in redis instead of in memory

--- server/src/shared/middleware/session.ts
+++ server/src/shared/middleware/session.ts
@@ -1,14 +1,27 @@
+import redis from 'redis';
 import session from 'express-session';
+import connectRedis from 'connect-redis';
 
-// TODO store sessions in redis instance provided by VIAA
+import { checkRequiredEnvs } from '../helpers/env-check';
+
+checkRequiredEnvs([
+	'COOKIES_SECRET',
+	'REDIS_CONNECTION_STRING',
+]);
+
+const redisStore = connectRedis(session);
+const redisClient = redis.createClient({
+	url: process.env.REDIS_CONNECTION_STRING,
+});
 
 export default session({
-	resave: false, // Postgres session provider doesn't need to resave the session every time
+	store: new redisStore({ client: redisClient }),
+	secret: process.env.COOKIES_SECRET,
+	resave: false, // Redis session provider doesn't need to resave the session every time
 	saveUninitialized: true, // Do not create a session for users that are not logged in, neither for health checks
 	cookie: {
 		secure: false, // TODO make secure once app runs on https: https://www.npmjs.com/package/express-session#cookiesecure
 		httpOnly: false,
 		maxAge: parseInt(process.env.COOKIES_MAXAGE as string, 10) || 4 * 60 * 60 * 1000, // 4h
 	},
-	secret: process.env.COOKIES_SECRET || '',
 });
