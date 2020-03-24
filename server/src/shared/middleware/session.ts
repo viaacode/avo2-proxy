5c8aa58e9c5cdde6d9f848d56ce66f56e6e89c2d
Bert Verhelst <verhelstbert@gmail.com>
2020-03-24 13:42:27 +0100

    fix(session): use redis when not local development

--- server/src/shared/middleware/session.ts
+++ server/src/shared/middleware/session.ts
@@ -4,24 +4,30 @@ import connectRedis from 'connect-redis';
 
 import { checkRequiredEnvs } from '../helpers/env-check';
 
-checkRequiredEnvs([
-	'COOKIES_SECRET',
-	'REDIS_CONNECTION_STRING',
-]);
-
-const redisStore = connectRedis(session);
-const redisClient = redis.createClient({
-	url: process.env.REDIS_CONNECTION_STRING,
-});
-
-export default session({
-	store: new redisStore({ client: redisClient }),
-	secret: process.env.COOKIES_SECRET,
-	resave: false, // Redis session provider doesn't need to resave the session every time
+const sessionConfig: session.SessionOptions = {
+	resave: false, // Postgres session provider doesn't need to resave the session every time
 	saveUninitialized: true, // Do not create a session for users that are not logged in, neither for health checks
 	cookie: {
 		secure: false, // TODO make secure once app runs on https: https://www.npmjs.com/package/express-session#cookiesecure
 		httpOnly: false,
 		maxAge: parseInt(process.env.COOKIES_MAXAGE as string, 10) || 4 * 60 * 60 * 1000, // 4h
 	},
-});
+	secret: process.env.COOKIES_SECRET || '',
+};
+
+if (process.env.NODE_ENV !== 'local') {
+	// Use the redis database as session storage
+	checkRequiredEnvs([
+		'COOKIES_SECRET',
+		'REDIS_CONNECTION_STRING',
+	]);
+
+	const redisStore = connectRedis(session);
+	const redisClient = redis.createClient({
+		url: process.env.REDIS_CONNECTION_STRING,
+	});
+
+	sessionConfig.store = new redisStore({ client: redisClient });
+}
+
+export default session(sessionConfig);
