5297af2dcdad679e1298fb041cef1553f1aa86e5
Bert Verhelst <verhelstbert@gmail.com>
2020-09-15 12:46:14 +0200

    fix(mam-syncrator): add error if mam env variables are not set

--- server/src/modules/mam-syncrator/mam-syncrator.service.ts
+++ server/src/modules/mam-syncrator/mam-syncrator.service.ts
@@ -1,11 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
 
-import { ExternalServerError } from '../../shared/helpers/error';
+import { ExternalServerError, InternalServerError } from '../../shared/helpers/error';
 
 export default class MamSyncratorService {
 	public static async triggerDeltaSync(): Promise<string> {
 		let url: string;
 		try {
+			if (!process.env.SYNCRATOR_API || !process.env.SYNCRATOR_ENV) {
+				throw new InternalServerError(
+					'Environment variables SYNCRATOR_API and SYNCRATOR_ENV are required'
+				);
+			}
 			url = `${process.env.SYNCRATOR_API}/delta/avo/${process.env.SYNCRATOR_ENV}`;
 			const response: AxiosResponse = await axios({
 				url,
