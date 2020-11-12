339d9a1593bbbea3832b3ac37309ce99c873af44
Bert Verhelst <verhelstbert@gmail.com>
2020-11-12 17:12:58 +0100

    feat(auth-service): use singular user groups proprty

--- server/src/modules/auth/service.ts
+++ server/src/modules/auth/service.ts
@@ -73,27 +73,26 @@ export class AuthService {
 			((user as unknown) as Avo.User.User).profile = (user.profile || {}) as Avo.User.Profile;
 			const permissions = new Set<string>();
 			const userGroupIds: number[] = [];
-			get(user, 'profile.profile_user_groups', []).forEach((profileUserGroup: any) => {
-				get(profileUserGroup, 'groups', []).forEach((userGroup: any) => {
-					userGroupIds.push(userGroup.id);
-					get(userGroup, 'group_user_permission_groups', []).forEach(
-						(permissionGroup: any) => {
-							get(
-								permissionGroup,
-								'permission_group.permission_group_user_permissions',
-								[]
-							).forEach((permission: any) => {
-								permissions.add(permission.permission.label);
-							});
-						}
-					);
-				});
-			});
+			const userGroup = get(user, 'profile.profile_user_group.group');
+			if (userGroup) {
+				userGroupIds.push(userGroup.id);
+				get(userGroup, 'group_user_permission_groups', []).forEach(
+					(permissionGroup: any) => {
+						get(
+							permissionGroup,
+							'permission_group.permission_group_user_permissions',
+							[]
+						).forEach((permission: any) => {
+							permissions.add(permission.permission.label);
+						});
+					}
+				);
+			}
 			(user as any).profile.userGroupIds = userGroupIds;
 			(user as any).profile.permissions = Array.from(permissions);
 			(user as any).idpmapObjects = user.idpmaps;
 			(user as any).idpmaps = uniq((user.idpmaps || []).map((obj) => obj.idp));
-			delete (user as any).profile.profile_user_groups;
+			delete (user as any).profile.profile_user_group;
 
 			// Simplify linked objects
 			(user as any).profile.educationLevels = (get(user, 'profile.profile_contexts', []) as {
